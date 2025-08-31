27218eff906a71f11e6c2d69d1d432a0ad859048
pvn <pvn>
2025-08-31 15:09:49 -0400

    add install instructions

--- build.cmake.sh
+++ build.cmake.sh
@@ -1,5 +1,8 @@
+mkdir -p installation/cppunit-1.15.1
 mkdir -p build
 pushd build
-cmake .. --fresh
+cmake .. -DCMAKE_INSTALL_PREFIX=../installation/cppunit-1.15.1 \
+--fresh
 cmake --build . --config Debug --parallel
+cmake --install . --config Debug
 popd 
